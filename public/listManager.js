ce02ea53563d973ae9839c254842c2476e3e212b
Dimitris Tsiktsiris <dtsiktsiris@outlook.com>
2026-01-24 13:36:15 +0200

    fix leftsidebar item onClick

--- public/listManager.js
+++ public/listManager.js
@@ -6,9 +6,13 @@ class ListItem {
 }
 
 let listItems = [
-  new ListItem("first Request", "https://jsonplaceholder.typicode.com/todos"),
-  new ListItem("second Request", "test"),
+  new ListItem("GET All", "https://jsonplaceholder.typicode.com/todos"),
+  new ListItem("GET Single", "https://jsonplaceholder.typicode.com/todos/2"),
 ];
+
+function listItemCardClicked(url) {
+  document.getElementById("urlInput").value = url;
+}
 
 const generateListElements = () => {
   let listItemCards = [];
@@ -16,6 +20,10 @@ const generateListElements = () => {
   for (let i = 0; i < listItems.length; i++) {
     tempCard = document.createElement("div");
     tempCard.setAttribute("class", "listItemCard");
+    tempCard.setAttribute(
+      "onClick",
+      `listItemCardClicked("${listItems[i].url.toString()}")`,
+    );
     tempCard.innerText = listItems[i].name;
     listItemCards.push(tempCard);
   }
